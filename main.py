8123dd366227deffdba4a4b7e7d6455e64a7c5e6
jwdavis0200 <jwdavis0200@gmail.com>
2023-12-27 19:21:00 -0800

    update to allow video download

--- main.py
+++ main.py
@@ -7,7 +7,12 @@ if __name__ == "__main__":
   url = str(input("What is the url you would like to download from youtube or soundcloud?"))
   if sc_or_yt.strip() == "y":
     ytdl_obj = Yt_dl(url)
-    ytdl_obj.download_audio()
+    v_or_a = str(input("Video[v] or audio[a]?"))
+    if v_or_a.strip() == "a":
+      ytdl_obj.download_audio()
+    elif v_or_a.strip() == 'v':
+      ytdl_obj.download_video()
+      
   elif sc_or_yt.strip() == "s":
     scdl_obj = Sc_dl(url)
     scdl_obj.download_audio()
--- modules/ytdl.py
+++ modules/ytdl.py
@@ -14,3 +14,20 @@ class Yt_dl:
     os.rename(out_file, new_file)
     print(self.yt.title + " has been downloaded to " + out_path )
     return
+  
+  def download_video(self):
+    video = self.yt.streams.filter(file_extension='mp4', res='1080p').first()
+    if video is None:
+      video = self.yt.streams.filter(file_extension='mp4', res='720p').first()
+    
+    out_path = os.path.join(os.path.expanduser("~"), "wtv_mp4")
+    try:
+      if video is None:
+        raise TypeError('No streams above 720p available!')
+      out_file = video.download(output_path=out_path)
+    except TypeError as e:
+      print(e)
+    base, ext = os.path.splitext(out_file)
+    new_file = base + ".mp4"
+    os.rename(out_file, new_file)
+    print(self.yt.title + " has been downloaded to " + out_path)
